621d4f0b0ca542c954d27de873b32665cb58d4e2
Joshua Ho <ho.joshua4@gmail.com>
2018-08-08 22:15:36 -0700

    Added hero and beginning styling integration with web components

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 Vue.component('vue-header', {
   template: `
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-light  " v-bind:style="headerStyle">
         <a class="navbar-brand" href="#">joshuaho.me</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -13,20 +13,41 @@ Vue.component('vue-header', {
           </div>
         </div>
       </nav>
-  `
-});
+  `,
+  data: () => {
+    return {
+      headerStyle: {
+        backgroundColor: '#24478f'
+      }
+    }
+  }
+})
 
 Vue.component('vue-intro', {
   template: `
-    <div class="container intro">
-      <h2>Front-end Developer, Digital Marketing Consultant & Small-Business Lover</h2>
+    <div class="container-fluid intro" v-bind:style="heroStyle">
+      <h1>Front-end Developer, Digital Marketing Consultant & Small-Business Lover</h1> <br />
       <h5>I love working with small buisness owners to help them accomplish their goals</h5>
     </div>
-  `
+  `,
+  data: () => {
+    return {
+      heroStyle: {
+        backgroundImage: 'url("./images/hero.jpeg")',
+        height: '80%',
+        textAlign: 'center',
+        padding: '200px 15%',
+        margin: '0'
+      },
+      heroText: {
+
+      }
+    }
+  }
 });
 Vue.component('vue-hello', {
   template: `
-    <div class="container hello">
+    <div class="container-fluid hello" v-bind:style="helloStyle">
         <h3>Hi I'm Josh. Glad to meet you.</h3>
         <span>Three years ago I completed the CodeFellows JavaScript
         bootcamp for Front End development.  Since, I've worked a variety of jobs
@@ -37,16 +58,45 @@ Vue.component('vue-hello', {
         e-commerce clients. I am naturally curious and creative,
         people oriented, and always looking to improve my skills.</span>
     </div>
-  `
+  `,
+  data: () => {
+    return {
+      helloStyle: {
+        textAlign: 'center',
+        padding: '200px 15%'
+      }
+    }
+  }
 })
-Vue.component('vue-recent', {
+Vue.component('vue-portfolio', {
   template: `
-  <div class="container recent-work ">
-    <h2>My Recent Work</h2>
-
-  </div>
-  `
+    <div class="container" v-bind:style="portfolioStyle">
+      <h2>My Recent Work</h2>
+    </div>
+  `,
+  data: ()=>{
+    return{
+      portfolioStyle:{
+          textAlign: 'center'
+      }
+    }
+  }
 });
+
+Vue.component('vue-portfolio-card', {
+  template: `
+    <div class="col-sm" v-bind:style="cardStyle">
+      <h1>I AM A PROJECT</h1>
+    </div>
+  `,
+  data: ()=>{
+    return {
+      cardStyle: {
+
+      }
+    }
+  }
+})
 Vue.component('vue-footer', {
   template: `
     <footer class="container-fluid footer">
